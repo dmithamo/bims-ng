f82c2bfb268a8f43fce55d7c4743fba95fa2dc48
dmithamo <33679184+dmithamo@users.noreply.github.com>
2023-11-22 10:35:13 +0300

    rethink envs::use a single file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'bims';
+  public version = environment.version;
 }
--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,8 +1,7 @@
 export const environment = {
-  env: 'development',
-
   // This is automatically rewritten by the update-version command via Git hooks
   version: 'GIT_HEAD_SHORT_HASH',
 
+  env: 'development',
   apiURL: 'https://api.example.com',
 };
