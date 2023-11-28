1cd0d59b9ac44af774362aafde26b9d21302cb20
dmithamo <33679184+dmithamo@users.noreply.github.com>
2023-11-28 14:35:12 +0300

    add redirect to logged-out guard

--- src/app/_guards/auth/logged-out.guard.spec.ts
+++ src/app/_guards/auth/logged-out.guard.spec.ts
@@ -2,11 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import {
   ActivatedRouteSnapshot,
   CanActivateFn,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
 
 import { loggedOutGuard } from './logged-out.guard';
 import { AuthService } from '../../_services/auth/auth.service';
+import { APP_ROUTE } from '../../shared/constants/routes.constants';
 
 describe('loggedOutGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
@@ -15,17 +17,23 @@ describe('loggedOutGuard', () => {
   let fakeAuthService: jasmine.SpyObj<AuthService>;
   let fakeActivatedRoute: ActivatedRouteSnapshot;
   let fakeRouterState: RouterStateSnapshot;
+  let fakeRouter: Router;
 
   beforeEach(() => {
     fakeAuthService = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
     fakeActivatedRoute = {} as ActivatedRouteSnapshot;
     fakeRouterState = {} as RouterStateSnapshot;
+    fakeRouter = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
       providers: [
         {
           provide: AuthService,
           useValue: fakeAuthService,
+        },
+        {
+          provide: Router,
+          useValue: fakeRouter,
         },
       ],
     });
@@ -35,10 +43,11 @@ describe('loggedOutGuard', () => {
     expect(executeGuard).toBeTruthy();
   });
 
-  it('should return false if there is a user logged in', async function () {
+  it('should return false if there is a user logged in and redirect to /dashboard', async function () {
     fakeAuthService.isLoggedIn.and.returnValue(Promise.resolve(true));
     const result = await executeGuard(fakeActivatedRoute, fakeRouterState);
 
+    expect(fakeRouter.navigate).toHaveBeenCalledWith([APP_ROUTE.dashboard]);
     expect(result).toBe(false);
   });
   it('should return true if there is no user logged in', async function () {
--- src/app/_guards/auth/logged-out.guard.ts
+++ src/app/_guards/auth/logged-out.guard.ts
@@ -1,9 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../../_services/auth/auth.service';
+import { APP_ROUTE } from '../../shared/constants/routes.constants';
 
 export const loggedOutGuard: CanActivateFn = async () => {
   const authService = inject(AuthService);
+  const router = inject(Router);
+  const isLoggedIn = await authService.isLoggedIn();
 
-  return !(await authService.isLoggedIn());
+  if (isLoggedIn) {
+    await router.navigate([APP_ROUTE.dashboard]);
+
+    return false;
+  }
+
+  return true;
 };
