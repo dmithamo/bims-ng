16986e74ef920f87019009cb68a568e92afc511b
dmithamo <33679184+dmithamo@users.noreply.github.com>
2023-12-04 14:52:38 +0300

    restyle the logo

--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { RouterLink } from '@angular/router';
+import { APP_ROUTE } from '../../constants/routes.constants';
 
 @Component({
   selector: 'app-logo',
   standalone: true,
-  imports: [CommonModule, SvgIconComponent],
+  imports: [CommonModule, SvgIconComponent, RouterLink],
   templateUrl: './logo.component.html',
 })
-export class LogoComponent {}
+export class LogoComponent {
+  protected readonly APP_ROUTE = APP_ROUTE;
+}
