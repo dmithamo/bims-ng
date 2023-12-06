fb74794a548ced278b8a92801574f23cc558f389
dmithamo <33679184+dmithamo@users.noreply.github.com>
2023-12-06 13:20:55 +0300

    run setup script locally only

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Use the node version specified in .nvmrc
-#!/bin/bash
-#source ~/.nvm/nvm.sh  # Adjust the path if your nvm installation is in a different location
-#nvm use // has to be run manually, because CI runs it too
+!/bin/bash
+source ~/.nvm/nvm.sh  # Adjust the path if your nvm installation is in a different location
+nvm use // has to be run manually, because CI runs it too
 
 # Copy the example environment file if it doesn't exist
 if [ ! -e "src/environments/environment.ts" ] ; then
@@ -10,10 +10,10 @@ if [ ! -e "src/environments/environment.ts" ] ; then
 fi
 
 # Create the SSL certificate if it doesn't exist
-if [ ! -e "local.bims.dmithamo.dev.pem" ] ; then
+if [ ! -e "local.bims.dmithamo.dev.pem" ]; then
   brew install mkcert nss # requires homebrew, and runs in sudo
   mkcert -install
   mkcert local.bims.dmithamo.dev
 fi
 
-pnpm run update-version
+bash run update-version
