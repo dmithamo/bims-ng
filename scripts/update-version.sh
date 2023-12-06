5437d4965a68a3ea46e7187598c89b5805a3223d
dmithamo <33679184+dmithamo@users.noreply.github.com>
2023-12-06 16:56:19 +0300

    undo my nonsense because the .env is re-written after each commit

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -e ".env" ] ; then
-  sed -i '' "s/NG_APP_VERSION=.*/NG_APP_VERSION=\'$(git rev-parse --short HEAD)\',/" .env
+  sed -i '' "s/NG_APP_VERSION=.*/NG_APP_VERSION=$(git rev-parse --short HEAD),/" .env
 fi
 
